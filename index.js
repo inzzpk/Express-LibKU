0683fcc36da63d1000ee58a8973368c20a3f5ffb
Pimvanat Kosiyakul <inzz.pk@gmail.com>
2017-04-18 22:20:35 +0700

    Create request

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 /* โหลด Express มาใช้งาน */
-var app = require('express')();
+var express = require('express');
+var bodyparser = require('body-parser');
 
 var users = require('./users');
-
 var mysqltest = require('./mysqltest');
- 
+
+var app = express()
+app.use(bodyparser.urlencoded({extened:true}));
+app.use(bodyparser.json());
 /* ใช้ port 7777 หรือจะส่งเข้ามาตอนรัน app ก็ได้ */
 var port = process.env.PORT || 7777;
  
@@ -94,6 +97,18 @@ app.get('/chkLogin/:user/:pass', function(req, res){
 	// res.json(cosmetic.searchByName(name));
 });
 
+app.post('/createRequest', function(req, res){
+	// console.log("IN REQUEST")
+	let json = req.body;
+	mysqltest.addRequest(json,function(err,result){
+		if (result){
+			console.log(result)
+			res.jon(result)
+		}
+	})
+	// res.json(cosmetic.searchByName(name));
+});
+
 app.get('/eieiei', function (req, res) {
 	mysqltest.addItems(function(err,result){
 		if (err){
@@ -111,3 +126,31 @@ app.get('/eieiei', function (req, res) {
 app.listen(port, function() {
     console.log('Starting node.js on port ' + port);
 });
+
+
+
+// app.get('/createRequest/:r_title/:r_type/:r_author/:r_isbn/:r_pub/:r_name/:r_mail/:r_tel/:r_fac/:r_year/:r_date' , function(req, res){
+
+// 	var r_title = req.params.r_title;
+// 	var r_type = req.params.r_type;
+// 	var r_author = req.params.r_author;
+// 	var r_isbn = req.params.r_isbn;
+// 	var r_pub = req.params.r_pub;
+// 	var r_name = req.params.r_name;
+// 	var r_mail = req.params.r_mail;
+// 	var r_tel = req.params.r_tel;
+// 	var r_fac = req.params.r_fac;
+// 	var r_year = req.params.r_year;
+// 	var r_date =  req.params.r_date;
+// 	mysqltest.addRequest(r_title,r_type,r_author,r_isbn,r_pub,r_name,r_mail,r_tel,r_fac,r_year,r_date,function(err,result){
+// 		if (err){
+// 			console.log(err);
+// 		}
+// 		else{
+// 			console.log(result)
+// 			res.json(result);
+// 		}
+		
+// 	});
+// 	// res.json(cosmetic.searchByName(name));
+// });
--- mysqltest.js
+++ mysqltest.js
@@ -65,6 +65,39 @@ exports.addItems = function(callback){
 	connection.end()	
 }
 
+exports.addRequest = function(json,callback){
+	console.log(json)
+	var mysql = require('mysql')
+	var connection = mysql.createConnection({
+		  host     : '158.108.48.254',
+		  user     : 'mobileapp',
+		  password : 'mobileapp',
+		  database : 'test'
+		});
+
+	connection.connect()
+	connection.query("INSERT INTO `req` (r_title,r_type,r_author,r_isbn,r_pub,r_name,r_mail,r_tel,r_event,r_cam,r_lib,r_fac,r_year,r_date,r_place,r_com,r_price,r_dep,r_pos,r_cnamber) VALUES (?,?,?,?,?,?,?,?,'recommend','Bangkhen','สำนักหอสมุด วิทยาเขตบางเขน',?,?,?,'','','','','','')",[json.r_title,json.r_type,json.r_author,json.r_isbn,json.r_pub,json.r_name,json.r_mail,json.r_tel,json.r_fac,json.r_year,json.r_date],function(err,result){
+		if (err)
+			console.log(err)
+		if (result){
+			console.log(result)
+
+		}
+
+	})
+	// connection.query("INSERT INTO `req` (r_title,r_type,r_author,r_isbn,r_pub,r_name,r_mail,r_tel,r_event,r_cam,r_lib,r_fac,r_year,r_date) VALUES (?,?,?,?,?,?,?,?,recommend,Bangkhen,สำนักหอสมุด วิทยาเขตบางเขน,?,?,?)"
+	// 	,[json.r_title,json.r_type,json.r_author,json.r_isbn,json.r_pub,json.r_name,json.r_mail,json.r_tel,json.r_fac,json.r_year,json.r_date], function (err, result){
+	//   if (err) {
+	//   	console.log(err)
+	// 	callback(err,null);
+	//   }
+	//   else{
+	//   	callback(null,result);
+	//   }
+	// })
+	connection.end()	
+}
+
 
 exports.queryItemsBarcode = function(barcode,callback){
 	var mysql = require('mysql')
